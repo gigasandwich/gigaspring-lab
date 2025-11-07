3ca5e1841e616c29e5f6f024156030a326978e76
Yvan Noah <yvannandy@gmail.com>
2025-11-07 15:52:41 +0300

    Add better tests for sprint 3

--- src/main/java/com/giga/springlab/App.java
+++ src/main/java/com/giga/springlab/App.java
@@ -1,16 +1,7 @@
 package com.giga.springlab;
 
-import com.giga.spring.annotation.*;
-import com.giga.springlab.annotation.AnnotatedClassLab;
-import com.giga.springlab.annotation.UrlMappingLab;
-import com.giga.springlab.controller.*;
-
 public class App {
     public static void main(String[] args) throws SecurityException {
-        UrlMappingLab.getInstance().displayAllUrlMappingPathValues(ControllerWithAnnotatedMethod.class);
-        
-        System.out.println("\n");
 
-        AnnotatedClassLab.getInstance().displayAnnotatedClasses(ControllerAnnotation.class, "com.giga.springlab");
     }
 }
--- src/main/java/com/giga/springlab/controller/AnnotatedController.java
+++ src/main/java/com/giga/springlab/controller/AnnotatedController.java
@@ -1,8 +1,21 @@
 package com.giga.springlab.controller;
 
 import com.giga.spring.annotation.ControllerAnnotation;
+import com.giga.spring.annotation.UrlMapping;
 
 @ControllerAnnotation
 public class AnnotatedController {
-    
+    @UrlMapping(path="/c0/path1")
+    private void methodWithAnnotation1() {
+
+    }
+
+    @UrlMapping(path="/c0/path2")
+    private void methodWithAnnotation2() {
+        
+    }
+
+    private void methodWithoutAnnotation() {}
+
+    private void anotherMethodWithoutAnnotation() {}
 }
