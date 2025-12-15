cd090becc44a18c6e4c90447e474805b4b3e3f80
Yvan Noah <yvannandy@gmail.com>
2025-12-15 14:50:11 +0300

    Add test for sprint 9

--- src/main/java/com/giga/springlab/model/Department.java
+++ src/main/java/com/giga/springlab/model/Department.java
@@ -1,5 +1,6 @@
 package com.giga.springlab.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -7,6 +8,7 @@ import lombok.ToString;
 @Data
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class Department {
     private String name;
 }
